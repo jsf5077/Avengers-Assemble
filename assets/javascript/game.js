29b000d8b0218711fafc2f07ca9a4fda4e0a987f
jsf5077 <jsf5077@gmail.com>
2019-05-07 00:54:06 -0400

    game fully functioning. Wins tracked when words complete. Losses tracked when guessesleft: 0. Still need a reset button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var word = ["IRON*MAN", "CAPTAIN*AMERICA", "MIGHTY*THOR", "INCREDIBLE*HULK", "BLACK*WIDOW", "NICK*FURY", "HAWKEYE"]
+var word = ["IRON*MAN", "CAPTAIN*AMERICA", "MIGHTY*THOR", "INCREDIBLE*HULK", "BLACK*WIDOW", "NICK*FURY", "HAWKEYE*"]
 var computerChoice = Math.floor(Math.random()*7);
 var answer = word[computerChoice];
 var wordLength = answer.length;
@@ -12,6 +12,7 @@ var wins = 0;
 var losses = 0;
 
 
+
 // variables that hold references to places in the HTML
 var directionsText = document.getElementById("directions-text");
 var winsText = document.getElementById("wins-text");
@@ -20,10 +21,11 @@ var guessText = document.getElementById("guesses-left");
 
  //console logs 
  console.log(answer);
- console.log(wordLength);
- console.log(guessesLeft);
+ console.log("WordLength: "+wordLength);
+ console.log("guessesLeft: "+guessesLeft);
  console.log("wins: " + wins);
  console.log("losses: " + losses);
+
 
 var layout = function() {
     // displays wins, losses, and guesses left
@@ -41,6 +43,7 @@ var layout = function() {
 
             }
             output = output + display[i];
+            
     }  
     // inserts the placeholders on page
     document.getElementById("game").innerHTML = output;
@@ -54,22 +57,35 @@ document.onkeyup = function(event) {
         userLetter = userKey;
         console.log("User Letter Chosen: " + userLetter)
         for (var w=0; w < answer.length; w++) {
-            // alert(letters[w])
             if (userLetter == letters[w]) {
                 display[w] = userLetter;
                 correct--;
                 console.log("letters remaining: " + correct);
             }
             output = output + display[w] + " ";
-            // output = "";
-            // guessesLeft--;
         }
         guessesLeft--;
-        guessText.textContent = "Guesses Left: " + guessesLeft;
-        console.log("Guesses Left: " + guessesLeft);
-        document.getElementById("game").innerHTML = output;
+        if (correct === 1) {
+            document.getElementById("victory").innerHTML = "YOU ARE VICTORIUS"
+            wins++;
+            winsText.textContent = "Wins: " + wins;
+            document.getElementById("game").innerHTML = output;
+        }
+        else if (guessesLeft < 1) {
+            document.getElementById("victory").innerHTML = "YOU HAVE BEEN DEFEATED!"
+            losses++;
+            lossesText.textContent = "Losses: " + losses;
+            document.getElementById("game").innerHTML = output;
+            guessText.textContent = "Guesses Left: " + 0;
+        }
+        else {
+            guessText.textContent = "Guesses Left: " + guessesLeft;
+            console.log("Guesses Left: " + guessesLeft);
+        }
+    document.getElementById("game").innerHTML = output;
     }
 }
+
 window.onload = function() {
     layout();
 }
