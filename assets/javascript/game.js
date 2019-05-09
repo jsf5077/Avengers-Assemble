00329b74d8a290ea257d2f346e5c8546d88b8b27
jsf5077 <jsf5077@gmail.com>
2019-05-08 23:09:14 -0400

    Complete overhaul of layout and script sctructure. Very Close. Letters don't repeat. Victory/lose messages and tallies update. game restarts after win or loss. Still need to stop repeated letters from affecting guesses left count

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
 var word = ["IRON*MAN", "CAPTAIN*AMERICA", "MIGHTY*THOR", "INCREDIBLE*HULK", "BLACK*WIDOW", "NICK*FURY", "HAWKEYE*"];
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var computerChoice = Math.floor(Math.random()*7);
-var answer = word[computerChoice];
-var wordLength = answer.length;
-var display = [wordLength];
-var correct = wordLength - 1;
-var letters = answer.split('');
-var guessesLeft = wordLength + 2; 
-var output = "";
-var userLetter = "";
+// var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// var computerChoice = Math.floor(Math.random()*7);
+// var answer = word[computerChoice];
+// var wordLength = answer.length;
+// var display = [wordLength];
+// var correct = wordLength - 1;
+// var letters = answer.split('');
+// var guessesLeft = wordLength + 2; 
+// var output = "";
+// var userLetter = "";
 var wins = 0;
 var losses = 0;
 var lettersGuessed = "";
@@ -22,16 +22,160 @@ var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
 var guessText = document.getElementById("guesses-left");
 
- //intial console logs 
- console.log(answer);
- console.log("WordLength: "+wordLength);
- console.log("guesses left: "+guessesLeft);
+ 
  console.log("wins: " + wins);
  console.log("losses: " + losses);
 
 
- var gameStart = function() {
+//  var gameStart = function() {
+//     //Declare an array for variable lettersGuessed
+//     lettersGuessed = [];
+//     // displays wins, losses, and guesses left
+//     winsText.textContent = "Wins: " + wins;
+//     lossesText.textContent = "Losses: " + losses;
+//     guessText.textContent = "Guesses Left: " + guessesLeft;
+//     // creates underscore placeholders for the length of chosen word
+//         for (var i=0; i < answer.length; i++) {
+//             if (answer[i] === "*") {
+//                 //there's probably a better way, but it works. So until I find a better way...
+//                 display[i] = "&nbsp; &nbsp; &nbsp;";
+//             }
+//             else {
+//                 display[i] = "_ &nbsp;";
+
+//             }
+//             output = output + display[i];
+            
+//     }  
+//     // inserts the placeholders on page
+//     document.getElementById("game").innerHTML = output;
+//     //writes initial letters guessed on page
+//     document.getElementById("letters-guessed").innerHTML= "--";
+//     //reset the output
+//     output ="";
+// }
+
+//function determines if the game has been won, lost, or need to continue guessing
+//also controls the letters guessed
+
+function keyGuessed() {
+    if (lettersGuessed.indexOf(userLetter) > -1) {    
+        for (var s = 0; s < wordLength; s++) {
+            document.getElementById("guess-message").innerHTML = "you've already guessed this letter!";
+            guessesLeft.classList.add("text-warning");   
+        }
+    }
+    else {
+        lettersGuessed.push(userLetter);
+        document.getElementById("letters-guessed").innerHTML = lettersGuessed.join(', ');
+        console.log("Letters guessed: " + userLetter);
+    }
+}
+
+// function winCalc() {
+//     if (correct < 1) {
+//         document.getElementById("victory").innerHTML = "YOU ARE VICTORIUS"
+//         wins++;
+//         winsText.textContent = "Wins: " + wins;
+//         document.getElementById("game").innerHTML = output;
+//     }
+//     else if (guessesLeft < 1) {
+//         document.getElementById("victory").innerHTML = "YOU HAVE BEEN DEFEATED!"
+//         losses++;
+//         lossesText.textContent = "Losses: " + losses;
+//         document.getElementById("game").innerHTML = output;
+//         guessText.textContent = "Guesses Left: " + 0;
+//     }
+//     else {
+//         guessText.textContent = "Guesses Left: " + guessesLeft;
+//         console.log("Guesses Left: " + guessesLeft);
+//         if (lettersGuessed === userLetter) {
+//             return;
+//         }
+//         else {
+//         keyGuessed();
+//         document.getElementById("game").innerHTML = output;
+//         }
+//     }
+// }   
+
+// //function controls when key is pressed  
+// document.onkeyup = function(event) {
+//     if (event.keyCode >= 65 && event.keyCode <= 90) {
+//         var userKey = event.key.toUpperCase();
+//         output = "";
+//         userLetter = userKey;
+//         // console.log("User Letter Chosen: " + userLetter)
+//         for (var w=0; w < answer.length; w++) {
+//             if (userLetter == letters[w]) {
+//                 display[w] = userLetter;
+//                 correct--;
+//                 console.log("letters remaining: " + correct);
+//             }
+//             output = output + display[w] + " ";
+//         }
+//         guessesLeft--;
+//         winCalc();
+//     }
+//     document.getElementById("game").innerHTML = output;
+// }
+
+
+
+// window.onload = function() {
+//     gameStart();
+// }
+
+
+
+
+// if (lettersGuessed.indexOf(userLetter) > -1) {    
+//     for (var s = 0; s < wordLength; s++) {
+//         if (display[s] = userLetter) {
+//             document.getElementById("guess-message").innerHTML = "you've already guessed this letter!";
+//             guessesLeft.classList.add("text-warning");   
+//         }
+//         output = output + display[s] + " ";
+//     }
+
+
+
+// ***Start Game***
+window.onload = function() {
+    gameStart();
+}
+//     computer picks a random name from Array
     //Declare an array for variable lettersGuessed
+var gameStart = function() {
+
+    //Declare Variables
+    var computerChoice = Math.floor(Math.random()*7);
+    var answer = word[computerChoice];
+    var wordLength = answer.length;
+    var display = [wordLength];
+    var correct = wordLength - 1;
+    var letters = answer.split('');
+    var guessesLeft = wordLength + 2; 
+    var output = "";
+    var userLetter = "";
+
+    //intial console logs 
+    console.log(answer);
+    console.log("WordLength: "+wordLength);
+    console.log("guesses left: "+guessesLeft);
+
+    if (lettersGuessed.indexOf(userLetter) > -1) {    
+        for (var s = 0; s < wordLength; s++) {
+            if (display[s] = userLetter) {
+                document.getElementById("guess-message").innerHTML = "you've already guessed this letter!";
+                guessesLeft.classList.add("text-warning");   
+            }
+        output = output + display[s] + " ";
+        }
+    }    
+
+    //Declare an array for variable lettersGuessed
+    console.log("gameStart")
     lettersGuessed = [];
     // displays wins, losses, and guesses left
     winsText.textContent = "Wins: " + wins;
@@ -39,110 +183,137 @@ var guessText = document.getElementById("guesses-left");
     guessText.textContent = "Guesses Left: " + guessesLeft;
     // creates underscore placeholders for the length of chosen word
         for (var i=0; i < answer.length; i++) {
+            //if the index value in answer equals * then displat three spaces.
             if (answer[i] === "*") {
-                //there's probably a better way, but it works. So until I find a better way...
+                //then display three spaces.
                 display[i] = "&nbsp; &nbsp; &nbsp;";
             }
+            //otherwise 
             else {
+                //display an underscore with a space
                 display[i] = "_ &nbsp;";
 
             }
+            // make output equal the the corresponding underscores and spaces from the for loop
             output = output + display[i];
             
     }  
-    // inserts the placeholders on page
+    // inserts the output back to the div with game id
     document.getElementById("game").innerHTML = output;
-    //writes initial letters guessed on page
+    //initialize the letters-guessed id with two dashes before guesses get put in. 
     document.getElementById("letters-guessed").innerHTML= "--";
-    //reset the output
+    //reset the output to equal nothing
     output ="";
-}
 
-//function determines if the game has been won, lost, or need to continue guessing
-//also controls the letters guessed
 
-function keyGuessed() {
-    lettersGuessed.push(userLetter);
-        document.getElementById("letters-guessed").innerHTML = lettersGuessed.join(', ');
-        console.log("Letters guessed: " + userLetter);
-}
+    // ***user has to guess word***
+    //function controls when key is pressed  
+    document.onkeyup = function(event) {
 
-function winCalc() {
-    if (correct < 1) {
-        document.getElementById("victory").innerHTML = "YOU ARE VICTORIUS"
-        wins++;
-        winsText.textContent = "Wins: " + wins;
-        document.getElementById("game").innerHTML = output;
-    }
-    else if (guessesLeft < 1) {
-        document.getElementById("victory").innerHTML = "YOU HAVE BEEN DEFEATED!"
-        losses++;
-        lossesText.textContent = "Losses: " + losses;
-        document.getElementById("game").innerHTML = output;
-        guessText.textContent = "Guesses Left: " + 0;
-    }
-    else {
-        guessText.textContent = "Guesses Left: " + guessesLeft;
-        console.log("Guesses Left: " + guessesLeft);
-        if (lettersGuessed === userLetter) {
-            return;
-        }
-        else {
-        keyGuessed();
-        document.getElementById("game").innerHTML = output;
-        }
-    }
-}   
+        //if key pressed matches a letter on the keyboard
+        if (event.keyCode >= 65 && event.keyCode <= 90) {
+            //variable userKey is equal to the letter pressed capitalized
+            var userKey = event.key.toUpperCase();
+            //output reset
+            output = "";
+            //global variable userLetter is set to equal local variable userKey
+            userLetter = userKey;
+            // console.log("User Letter Chosen: " + userLetter)
 
-//function controls when key is pressed  
-document.onkeyup = function(event) {
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
-        var userKey = event.key.toUpperCase();
-        output = "";
-        userLetter = userKey;
-        // console.log("User Letter Chosen: " + userLetter)
-        for (var w=0; w < answer.length; w++) {
-            if (userLetter == letters[w]) {
-                display[w] = userLetter;
-                correct--;
-                console.log("letters remaining: " + correct);
+            //for loop that funs for the length of answer
+            for (var w=0; w < answer.length; w++) {
+                //if the key pressed by the user equals one of the letters in the answer
+                if (userLetter == letters[w]) {
+                    //change the index at the same display to equal the user guess
+                    display[w] = userLetter;
+                    //subtract from letters remaining
+                    correct--;
+                    console.log("letters remaining: " + correct);
+                }
+                //change output to the display index value obtained from the if statement
+                output = output + display[w] + " ";
             }
-            output = output + display[w] + " ";
+            // subtract from guesses left for the user
+            guessesLeft--;
+            // run winCalc function
+            winCalc();
         }
-        guessesLeft--;
-        winCalc();
+        // update the html with the game id with the new output
+        document.getElementById("game").innerHTML = output;
     }
-    document.getElementById("game").innerHTML = output;
+
+
+    //this determines in the user has completed the game 
+    function winCalc() {
+        // if all the letters have been guessed
+        if (correct < 1) {
+            // display victory in html div id victory
+            document.getElementById("victory").innerHTML = "YOU ARE VICTORIUS"
+            //update the wins variable by 1
+            wins++;
+            //update the wins in the html
+            winsText.textContent = "Wins: " + wins;
+            //updates the value in the game html, but do I have to at this point?///////////////////////////////////////////////////////////
+            document.getElementById("game").innerHTML = output;
+            gameStart();
+        }
+        //If the user ran out of guesses left...
+        else if (guessesLeft < 1) {
+            //update the html id victory to say the user lost
+            document.getElementById("victory").innerHTML = "YOU HAVE BEEN DEFEATED!"
+            //update losses
+            losses++;
+            //display update to losses in html
+            lossesText.textContent = "Losses: " + losses;
+            //updates the value in the game html, but do I have to at this point?/////////////////////////////////////////////////////////// 
+            document.getElementById("game").innerHTML = output;
+            // update the guesses left to override the fact that there is a * value in the index
+            guessText.textContent = "Guesses Left: " + 0;
+            gameStart();
+        }
+        // if the user still has guesses and there are still letters to guess in the word...
+        else {
+            //update guesses left... but not sure at the moment where its pulling that from/////////////////////////////////////////////////
+            guessText.textContent = "Guesses Left: " + guessesLeft;
+            console.log("Guesses Left: " + guessesLeft);
+            // if user is guessing a letter already guessed
+            if (lettersGuessed === userLetter) {
+                //return nothing
+                return;
+            }
+            //otherwise...
+            else {
+            // run keyguessed function. should this be here?///////////////////////////////////////////////////////////////////////////////
+            keyGuessed();
+            // update the game html
+            document.getElementById("game").innerHTML = output;
+            }
+        }
+        
+    } 
+
+    //supposed to determine if a letter has already been guessed
+    function keyGuessed() {
+        //if the userletter matches any letter in the index of lettersGuessed...
+        if (lettersGuessed.indexOf(userLetter) > -1) {
+            // run for loop cycling through wordLength which is the same thing as answer.length    
+            for (var s = 0; s < wordLength; s++) {
+                // update html id guess-message to tell them theyve already guessed this letter
+                document.getElementById("guess-message").innerHTML = "you've already guessed this letter!";
+                // not sure if needed
+                // guessesLeft.classList.add("text-warning");   
+            }
+        }
+        // otherwise...
+        else {
+            // enter the letter guessed by the user into lettersGuessed array
+            lettersGuessed.push(userLetter);
+            //update the html id 
+            document.getElementById("letters-guessed").innerHTML = lettersGuessed.join(', ');
+            console.log("Letters guessed: " + userLetter);
+        }
+    }
 }
-
-
-
-window.onload = function() {
-    gameStart();
-}
-
-
-
-
-// if (alphabet.indexOf(userLetter) > -1) {    
-//     for (var s = 0; s < wordLength; s++) {
-//         if (display[w] = userLetter) {
-//             document.getElementById("guess-message").innerHTML = "you've already guessed this letter!";
-//             guessesLeft.classList.add("text-warning");   
-//         }
-//         output = output + display[w] + " ";
-//     }
-
-
-
-// ***Start Game***
-//     computer picks a random name from Array
-//     placeholders populate with the amount of letters in the chosen word
-
-// ***user has to guess word***
-
-// when user guess correct letter in word 
-//     placeholder for word populates with appropriate letter
 
 // when user guess wrong letter in word
 //     letter appears at the bottom in a list 
